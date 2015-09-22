56e1550809e48f72dd19458aebcd8d93abce6136
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2015-09-22 11:34:17 -0400

    add remove empty p

--- assets/themes/kyaneos/lib/inc/msd-functions.php
+++ assets/themes/kyaneos/lib/inc/msd-functions.php
@@ -206,3 +206,23 @@ function get_attachment_id_from_src ($image_src) {
 
     }
 }
+
+add_filter( 'the_content', 'remove_empty_p', 20, 1 );
+function remove_empty_p( $content ){
+    // clean up p tags around block elements
+    $content = preg_replace( array(
+        '#<p>\s*<(div|aside|section|article|header|footer)#',
+        '#</(div|aside|section|article|header|footer)>\s*</p>#',
+        '#</(div|aside|section|article|header|footer)>\s*<br ?/?>#',
+        '#<(div|aside|section|article|header|footer)(.*?)>\s*</p>#',
+        '#<p>\s*</(div|aside|section|article|header|footer)#',
+    ), array(
+        '<$1',
+        '</$1>',
+        '</$1>',
+        '<$1$2>',
+        '</$1',
+    ), $content );
+
+    return preg_replace('#<p>(\s|&nbsp;)*+(<br\s*/*>)*(\s|&nbsp;)*</p>#i', '', $content);
+}
